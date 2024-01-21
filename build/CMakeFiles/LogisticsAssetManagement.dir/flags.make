3596a466af6f3e7bcddacde5f0fbfc476f468060
 HongjuanJiang-Ada <jianghongjuan1996@gmail.com>
2024-01-21 20:02:27 +0100

    Get the asset detail from the keyboard input

--- build/CMakeFiles/LogisticsAssetManagement.dir/flags.make
+++ build/CMakeFiles/LogisticsAssetManagement.dir/flags.make
@@ -6,7 +6,7 @@ CXX_DEFINES =
 
 CXX_INCLUDES = -I/usr/local/mysql-connector-c++/include/jdbc -I/Users/ada/Downloads/LogisticsBackend/LogisticsAssetManagement/include
 
-CXX_FLAGSarm64 = -std=gnu++11 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk
+CXX_FLAGSarm64 = -std=gnu++17 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk
 
-CXX_FLAGS = -std=gnu++11 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk
+CXX_FLAGS = -std=gnu++17 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include "AssetManager.h"
+#include <iostream>
+
+using namespace std;
+
+//Function prototype
+tuple<string, string, string> getAssetDetails();
 
 int main() {
-    // Create an instance of AssetManager
+    auto [name, category, purchase_date] = getAssetDetails();
+
+    // Create an instance of AssetManager and add the asset
     AssetManager manager;
-    manager.addAsset("Airpods", "Electronics", "2020-03-01");
+    manager.addAsset(name, category, purchase_date);
+
     return 0;
+}
+
+//Function definition
+tuple<string, string, string> getAssetDetails(){
+    string name, category, purchase_date;
+
+    cout << "Please enter the asset name: ";
+    getline(cin, name);
+
+    cout << "Please enter the asset category: ";
+    getline(cin, category);
+
+    cout << "Please enter the asset purchase date (YYYY-MM-DD): ";
+    getline(cin, purchase_date);
+
+    return {name, category, purchase_date};
 }
